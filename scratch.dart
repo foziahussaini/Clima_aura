8f1da8c93ac8b80c7ee8e0a8fc8d2619cbb574bb
foziahussaini <fauziakhudadad@gmail.com>
2026-04-10 07:21:34 +0430

    async usage

---
+++ scratch.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+
+import 'dart:io';
+
+void main(){
+  performTask();
+}
+
+void performTask()async {
+  task1();
+  String task2Data =await task2();
+  task3(task2Data);
+}
+
+void task1(){
+  String result = "task 1 date";
+  print("task 1 completed");
+}
+
+Future<String> task2()async {
+  Duration threeSeconds = Duration(seconds: 3);
+  // sleep(threeSeconds);
+  String? result;
+  await Future.delayed(threeSeconds, (){
+    String result = "task 2 date";
+    print("task 2 completed");
+  });
+  return result!;
+}
+
+void task3(String task2Data){
+  String result = "task 3 date";
+  print("task 3 completed $task2Data");
+}
